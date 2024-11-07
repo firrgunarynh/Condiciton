317708e9c74f9d8d75e1aadc5c7e03297e39ad9d
firrgunarynh <firguna208@gmail.com>
2024-11-07 13:32:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("orderButton").onclick = function () {
+    let foodPrice = parseInt(document.getElementById ("food").value);
+    let drinkPrice = parseInt(document.getElementById ("drink").value);
+
+    let totalPrice = foodPrice + drinkPrice;
+
+   let discount = 0;
+
+   if (totalPrice > 90) {
+    alert('Ok');
+   }
+}
